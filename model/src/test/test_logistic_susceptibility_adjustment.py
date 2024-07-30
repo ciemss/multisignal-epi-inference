86a6c152ffb3c7723bfc053d9e07fafa28c96fae
agustin.kruel@pnnl.gov <agustin.kruel@pnnl.gov>
2024-07-30 16:26:53 -0700

    Make new_I_raw a torch tensor, and use torch.exp instead of jnp.exp.

--- model/src/test/test_logistic_susceptibility_adjustment.py
+++ model/src/test/test_logistic_susceptibility_adjustment.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 # numpydoc ignore=GL08
 
-import jax.numpy as jnp
+import torch
 from pyrenew.latent import logistic_susceptibility_adjustment
 
 
 def test_logistic_susceptibility_adjustment():  # numpydoc ignore=GL08
-    new_I_raw = 1000000
+    new_I_raw = torch.tensor([1000000])
     population = 100
 
     assert (
@@ -16,8 +16,8 @@ def test_logistic_susceptibility_adjustment():  # numpydoc ignore=GL08
 
     assert logistic_susceptibility_adjustment(new_I_raw, 0, population) == 0
 
-    new_I_raw = 7.2352
+    new_I_raw = torch.tensor([7.2352])
     assert (
         logistic_susceptibility_adjustment(new_I_raw, 0.75, population)
-        == (1 - jnp.exp(-new_I_raw / population)) * 0.75 * population
+        == (1 - torch.exp(-new_I_raw / population)) * 0.75 * population
     )
